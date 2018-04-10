0912b2621bdbdf99800b5ab9c15e24eab7d2d099
1WHNNUDRMKZF3QN\Administrator <qwqwdvef@gmail.com>
2018-04-11 00:22:26 +0800

    初始化springboot项目

---
+++ ctp-web/src/main/java/com/ctp/web/CtpApplication.java
@@ -0,0 +1,12 @@
+package com.ctp.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtpApplication.class, args);
+	}
+}
---
+++ ctp-web/src/test/java/com/ctp/web/CtpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ctp.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CtpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
